3b2157e79551ca8572d950ffb91c171945104532
noahcoolboy <46800081+noahcoolboy@users.noreply.github.com>
2020-03-15 00:46:58 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -24,7 +24,7 @@
 
 local import = function(toimport)
     if type(toimport) == "string" then
-        return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/nrv-ous/Hydroxide/master/" .. toimport))()
+        return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/noahcoolboy/Hydroxide/master/" .. toimport))()
     else 
         return game:GetObjects("rbxassetid://" .. toimport)[1]
     end
